a402366d91d6a2aaa96851ddc0b28799251ed58e
Matthew Tao <goatatia@gmail.com>
2021-09-03 17:46:12 +1200

    styled home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,22 @@ import Shishiro_Botan from "../images/Shishiro_Botan.png";
 
 const Home = () => {
 	return (
-		<main className="home">
-			<div className="botan_wrapper">
-				<img className="botan_img" src={Shishiro_Botan} alt="botan_img" />
-				<div className="desc_text">
-					<p className="botan_info">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-						luctus ipsum et finibus faucibus. Phasellus eu sodales elit.
-						Pellentesque sed porttitor lectus. Proin et nisl elementum, cursus
-						nisl at, feugiat lacus.
-					</p>
-				</div>
+		<main id="home">
+			<div className="welcome-text">
+				<h2>Welcome!</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris luctus
+					ipsum et finibus faucibus. Phasellus eu sodales elit. Pellentesque sed
+					porttitor lectus. Proin et nisl elementum, cursus nisl at, feugiat
+					lacus.
+				</p>
+			</div>
+			<div className="botan-img-container">
+				<img
+					className="botan-img"
+					src={Shishiro_Botan}
+					alt="A full body shot of Botan in her original outfit."
+				/>
 			</div>
 		</main>
 	);
