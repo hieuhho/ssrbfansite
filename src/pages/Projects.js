8fcc70d9ad034093f9559708c2315756e6283f75
Matthew Tao <goatatia@gmail.com>
2021-09-03 16:03:47 +1200

    fixed projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
 import { MenuItems } from "../components/navbar/MenuItems";
 import { Link } from "react-router-dom";
 
 const Projects = () => {
-	const [click, setClick] = useState(false);
-	const handleClick = () => setClick(!click);
-
 	return (
-		<main>
-			<h1 className="projects" onClick={handleClick}>
-				{MenuItems.map((item, index) => {
-					return (
-						<Link
-							className="project_col"
-							to={item.path}
-							onClick={() => setClick(false)}
-							style={{
-								backgroundImage: 'url("' + item.img + '")',
-							}}
-							data-text={item.desc}
-						>
-							<div className="project_title">{item.title}</div>
-						</Link>
-					);
-				})}
-			</h1>
+		<main id="projects">
+			{MenuItems.map((item, index) => {
+				return (
+					<Link to={item.path}>
+						<div className="project">
+							<img src={item.img} />
+							<h2>{item.title}</h2>
+							<div className="project-desc">
+								<p>{item.desc}</p>
+							</div>
+						</div>
+					</Link>
+				);
+			})}
 		</main>
 	);
 };
