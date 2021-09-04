35ccf8b569e0482d1856671e99dfb5b8246c266e
Matthew Tao <goatatia@gmail.com>
2021-09-04 15:06:53 +1200

    added animations to project cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,26 +1,24 @@
 import { MenuItems } from "../components/navbar/MenuItems";
 import { Link } from "react-router-dom";
+import title from "../images/botan.svg";
 
 const Projects = () => {
 	return (
 		<main id="projects">
+			<img className="welcome-header" src={title} alt="SHISHIRO BOTAN" />
 			<h1 className="title">Projects</h1>
 			<div className="projects-container">
 				{MenuItems.map((item, index) => {
 					return (
-						<div className="project">
-							<Link to={item.path}>
-								<img src={item.img} />
-							</Link>
-							<Link className="project-heading" to={item.path}>
-								<h2>{item.title}</h2>
-							</Link>
+						<Link className="project" to={item.path}>
+							<img src={item.img} />
+							<h2 className="project-heading">{item.title}</h2>
 							<div className="project-body">
 								<div className="project-desc">
 									<p>{item.desc}</p>
 								</div>
 							</div>
-						</div>
+						</Link>
 					);
 				})}
 			</div>
