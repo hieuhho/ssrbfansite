5f34487b223f0f65d078bf9a9ca9b2c1f5010dc3
Matthew Tao <goatatia@gmail.com>
2021-09-06 18:11:43 +1200

    implemented initial sidebar design

--- src/pages/Birthday.js
+++ src/pages/Birthday.js
@@ -76,18 +76,41 @@ const Birthday = () => {
 
 	return (
 		<div id="map">
-			{/* <button onClick={() => setTip("true")}>Tweet your SSRB</button>
-			<Modal title="Instructions" onClose={() => setTip(false)} show={tip}>
-				<p>Tweet this #SSRBWOrld</p>
-			</Modal> */}
-			<Modal
-				title={`${country.fans} SSRBmins from ${country.cName}`}
-				onClose={() => setShow(false)}
-				show={show}
-			></Modal>
 			<aside className="map-sidebar">
-				<p>Click a country to see the number of SSRBmins</p>
+				<div className="map-sidebar-header">
+					<div className="map-sidebar-header-title">
+						<h3>SSRB World Map</h3>
+						<button className="map-help-btn" onClick={() => setTip(true)}>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="24px"
+								viewBox="0 0 24 24"
+								width="24px"
+								fill="#FFFFFF"
+							>
+								<path d="M0 0h24v24H0V0z" fill="none" />
+								<path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z" />
+							</svg>
+						</button>
+					</div>
+					<p className="map-hint">
+						Click a country to see the number of SSRBmins
+					</p>
+				</div>
+				<svg
+					data-name="slantBottom"
+					class="slant small"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 1 1"
+					preserveAspectRatio="none"
+				>
+					<path fill="#303030" d="M0 0h1v1L0 0z" />
+				</svg>
+				<div className="map-sidebar-content">
+					<h4>New Zealand</h4>
+				</div>
 			</aside>
+
 			<VectorMap
 				map={"world_mill"}
 				backgroundColor="#0077be"
@@ -118,6 +141,14 @@ const Birthday = () => {
 					],
 				}}
 			/>
+			<Modal title="Instructions" onClose={() => setTip(false)} show={tip}>
+				<p>Tweet this #SSRBWOrld</p>
+			</Modal>
+			<Modal
+				title={`${country.fans} SSRBmins from ${country.cName}`}
+				onClose={() => setShow(false)}
+				show={show}
+			></Modal>
 		</div>
 	);
 };
