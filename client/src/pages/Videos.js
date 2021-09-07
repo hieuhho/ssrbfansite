abf8a53f1d06fd43a91ece951b4a4cca4390e682
Matthew Tao <goatatia@gmail.com>
2021-09-08 08:18:47 +1200

    changed videos heading to subheading

--- client/src/pages/Videos.js
+++ client/src/pages/Videos.js
@@ -1,21 +1,20 @@
 import React from "react";
-import ReactPlayer from 'react-player'
-
+import ReactPlayer from "react-player";
 
 const Videos = () => {
 	return (
 		<main>
-			<h1 className="videos">Videos</h1>
-			<div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url='https://www.youtube.com/playlist?list=PLHLU3OPWJVbG3tDTLznOZ1Iv9t-Pju5V4'
-          width='100%'
-          height='100%'
-					controls='true'
-					volume='0.3'
-        />
-      </div>
+			<h1 className="subheading">Videos</h1>
+			<div className="player-wrapper">
+				<ReactPlayer
+					className="react-player"
+					url="https://www.youtube.com/playlist?list=PLHLU3OPWJVbG3tDTLznOZ1Iv9t-Pju5V4"
+					width="100%"
+					height="100%"
+					controls="true"
+					volume="0.3"
+				/>
+			</div>
 		</main>
 	);
 };
