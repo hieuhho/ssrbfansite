8be6b531b37fd704644e4c40bf2a186fcb380d91
hieuhho <hieuhho.dev@gmail.com>
2021-09-06 20:09:49 -0700

    update env

--- create-env.js
+++ create-env.js
@@ -1,2 +1,4 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `DB_HOST=${process.env.DB_HOST}\nDB_PASS=${process.env.DB_PASS}\nDB_USER=${process.env.DB_USER}\nDB_DATABASE=${process.env.DB_DATABASE}\n`)
+fs.writeFileSync('./.env',
+`DB_HOST=${process.env.DB_HOST}\nDB_PASS=${process.env.DB_PASS}\nDB_USER=${process.env.DB_USER}\nDB_DATABASE=${process.env.DB_DATABASE}\n`
+);
