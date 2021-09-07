d639fb980db62b32a221473c1b1ef7cf55e2b125
Matthew Tao <goatatia@gmail.com>
2021-09-07 13:50:25 +1200

    reordered and commented on MenuItems.js

--- client/src/components/navbar/MenuItems.js
+++ client/src/components/navbar/MenuItems.js
@@ -1,37 +1,40 @@
-  export const MenuItems = [
-  {
-    title: "Anniversary",
-    img: "https://i.picsum.photos/id/230/400/900.jpg?hmac=AQI-uciC8LquKEeNWdA4W_SQkDjIIQmwfYb9HzFL7PU",
-    desc: "Make your own SSRBmin",
-    path: "/anniversary",
-    cName: "dropdown-link"
-  },
-  {
-    title: "Birthday (SSRBWorld)",
-    img: "https://i.picsum.photos/id/1039/400/900.jpg?hmac=K-7d_oihjW6jwBn8olpAFfhet_ISlyoTjh3sRVcSQDA",
-    desc: "Tweet your SSRB around the world",
-    path: "/birthday",
-    cName: "dropdown-link"
-  },
-  // {
-  //   title: "1M Milestone",
-  //   img: "https://i.picsum.photos/id/313/400/900.jpg?hmac=fVxZssWtwwnIp6Vs1iVfRxpsBXsk4eixcr18ncvwwY8",
-  //   desc: "Under Construction",
-  //   path: "/milestone",
-  //   cName: "dropdown-link"
-  // },
-  {
-    title: "Taiwan Project",
-    img: "https://i.picsum.photos/id/308/400/900.jpg?hmac=K-lTXDPeU9ZwtQscSDdJnjsVC33kPHBXUNGCDmx2k04",
-    desc: "Under Construction",
-    path: "/taiwan",
-    cName: "dropdown-link"
-  },
-  // {
-  //   title: "English Project",
-  //   img: "https://i.picsum.photos/id/863/400/900.jpg?hmac=zA6xx7JLzZjgRXHNsdB2ZQXIou3lZx70mI92i-yrKns",
-  //   desc: "Fusce auctor metus quis nulla consectetur, id venenatis neque lobortis. Donec ac placerat libero. Maecenas vestibulum egestas quam, a aliquet turpis fermentum eget. Vivamus odio lectus, euismod eu massa vel, iaculis pellentesque quam. Phasellus nec leo ligula.",
-  //   path: "/daipan",
-  //   cName: "dropdown-link"
-  // }
+// Projects in decending order of date.
+// This is because the top-most item is displayed on the home page as the most recent.
+// This is also the order in which the items are displayed on the project page.
+export const MenuItems = [
+	{
+		title: "Birthday (SSRBWorld)",
+		img: "https://i.picsum.photos/id/1039/400/900.jpg?hmac=K-7d_oihjW6jwBn8olpAFfhet_ISlyoTjh3sRVcSQDA",
+		desc: "Tweet your SSRB around the world",
+		path: "/birthday",
+		cName: "dropdown-link",
+	},
+	{
+		title: "Anniversary",
+		img: "https://i.picsum.photos/id/230/400/900.jpg?hmac=AQI-uciC8LquKEeNWdA4W_SQkDjIIQmwfYb9HzFL7PU",
+		desc: "Make your own SSRBmin",
+		path: "/anniversary",
+		cName: "dropdown-link",
+	},
+	// {
+	//   title: "1M Milestone",
+	//   img: "https://i.picsum.photos/id/313/400/900.jpg?hmac=fVxZssWtwwnIp6Vs1iVfRxpsBXsk4eixcr18ncvwwY8",
+	//   desc: "Under Construction",
+	//   path: "/milestone",
+	//   cName: "dropdown-link"
+	// },
+	{
+		title: "Taiwan Project",
+		img: "https://i.picsum.photos/id/308/400/900.jpg?hmac=K-lTXDPeU9ZwtQscSDdJnjsVC33kPHBXUNGCDmx2k04",
+		desc: "Under Construction",
+		path: "/taiwan",
+		cName: "dropdown-link",
+	},
+	// {
+	//   title: "English Project",
+	//   img: "https://i.picsum.photos/id/863/400/900.jpg?hmac=zA6xx7JLzZjgRXHNsdB2ZQXIou3lZx70mI92i-yrKns",
+	//   desc: "Fusce auctor metus quis nulla consectetur, id venenatis neque lobortis. Donec ac placerat libero. Maecenas vestibulum egestas quam, a aliquet turpis fermentum eget. Vivamus odio lectus, euismod eu massa vel, iaculis pellentesque quam. Phasellus nec leo ligula.",
+	//   path: "/daipan",
+	//   cName: "dropdown-link"
+	// }
 ];
