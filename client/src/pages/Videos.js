7f1cce074d1ad84ed7ed00ee754dcd48ebf6afed
Matthew Tao <goatatia@gmail.com>
2021-09-22 15:58:00 +1200

    fix iframe parameters

--- client/src/pages/Videos.js
+++ client/src/pages/Videos.js
@@ -11,9 +11,9 @@ const Videos = () => {
 						height="315"
 						src="https://www.youtube-nocookie.com/embed/videoseries?list=PLHLU3OPWJVbG3tDTLznOZ1Iv9t-Pju5V4"
 						title="YouTube video player"
-						frameborder="0"
+						frameBorder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen
+						allowFullScreen
 					></iframe>
 				</div>
 			</div>
